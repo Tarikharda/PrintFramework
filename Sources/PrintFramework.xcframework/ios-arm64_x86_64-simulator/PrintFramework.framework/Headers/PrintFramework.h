f0ebc85b5f3c9facf31d39aa0979078c690c2429
TarikHarda <tarikpy7@gmail.com>
2023-11-28 12:43:04 +0100

    version 0.0.2

---
+++ Sources/PrintFramework.xcframework/ios-arm64_x86_64-simulator/PrintFramework.framework/Headers/PrintFramework.h
@@ -0,0 +1,18 @@
+//
+//  PrintFramework.h
+//  PrintFramework
+//
+//  Created by mac book pro on 28/11/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PrintFramework.
+FOUNDATION_EXPORT double PrintFrameworkVersionNumber;
+
+//! Project version string for PrintFramework.
+FOUNDATION_EXPORT const unsigned char PrintFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PrintFramework/PublicHeader.h>
+
+
