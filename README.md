2c3a2f7f760dca009e4f3ed6a2f104ad0f477db1
TarikHarda <tarikpy7@gmail.com>
2023-11-28 09:30:19 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# PrintFramework
+
+A description of this package.
---
+++ Sources/PrintFramework/PrintFramework.swift
@@ -0,0 +1,6 @@
+public struct PrintFramework {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
