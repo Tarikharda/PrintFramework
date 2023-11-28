ccb55e2c5ee182eafe9cebc63ffe1a349715bff0
TarikHarda <tarikpy7@gmail.com>
2023-11-28 09:41:59 +0100

    First Commit

---
+++ Sources/PrintFramework/File.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by mac book pro on 28/11/2023.
+//
+
+import Foundation
+
+public struct File{
+    public static func doSomething(){
+        print("Hello")
+    }
+}
